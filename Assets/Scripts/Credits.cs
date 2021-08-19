209bf405e5d226f9dfa62fa4afaeb3973e9e926f
BowiePro <bowieproyt@gmail.com>
2021-08-19 06:31:14 -0400

    0.3

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
     }
 
 }
